2d15af9a5e4b074768be9452f2259313c2ffab9f
Guilherme Siqueira <siqueiraguilherme920@gmail.com>
2023-02-17 22:11:47 -0300

    Added content script

---
+++ script.js
@@ -0,0 +1,28 @@
+//Elements
+const calc_btn = document.querySelector('#calculate')
+
+//Functions
+function calculate() {
+    const name = document.querySelector('#name').value
+    const height = document.querySelector('#height').value
+    const weight = document.querySelector('#weight').value
+    let imc
+    if (height == '' || weight == '' || name == ''){
+        alert('Preencha todos os dados')
+    } else {
+        imc = (weight / (height * height)).toFixed(1)
+    }
+    return imc
+}
+
+//Events
+calc_btn.addEventListener('click', () => {
+    let imc = calculate()
+    if (imc <= 24.9 && imc > 18.5) {
+        console.log('Peso normal')
+    } else if (imc <= 18.5) {
+        console.log('Abaixo do peso')
+    } else {
+        console.log('Acima do peso')
+    }
+})
